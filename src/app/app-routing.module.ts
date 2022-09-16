71078cd8d3569c23701f61b4ddb87065553ddae0
maximenorval <maximenorval@gmail.com>
2022-09-16 16:21:56 +0200

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './core/home/home.component';
+import { PetListComponent } from './pet/pet-list/pet-list.component';
+import { AuthenticationComponent } from './user/authentication/authentication.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: AuthenticationComponent },
+      { path: 'signup', component: AuthenticationComponent },
+    ],
+  },
+  {
+    path: 'pet',
+    children: [{ path: 'index', component: PetListComponent }],
+  },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
