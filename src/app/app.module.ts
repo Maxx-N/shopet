d2e7fcecc27a8865ba770ce14cc02e121c7e63ce
maximenorval <maximenorval@gmail.com>
2022-09-16 15:30:35 +0200

    creating authentication component and user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetListComponent } from './pet/pet-list/pet-list.component';
+import { AuthenticationComponent } from './user/authentication/authentication.component';
 
 @NgModule({
-  declarations: [AppComponent, PetListComponent],
+  declarations: [AppComponent, PetListComponent, AuthenticationComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
