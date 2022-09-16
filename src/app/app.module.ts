211e748eadac90f5ad0e8fd496548521c99fd19b
maximenorval <maximenorval@gmail.com>
2022-09-16 16:16:06 +0200

    creating a navbar and a home component + adding angular material and flex layout + main layout in the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetListComponent } from './pet/pet-list/pet-list.component';
 import { AuthenticationComponent } from './user/authentication/authentication.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { HomeComponent } from './core/home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [AppComponent, PetListComponent, AuthenticationComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PetListComponent,
+    AuthenticationComponent,
+    NavbarComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
