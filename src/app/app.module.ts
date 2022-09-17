791d5d9487bef49204807c049e8507cd1bac710c
maximenorval <maximenorval@gmail.com>
2022-09-17 09:50:07 +0200

    getting rid of unused FoormModule import in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PetListComponent } from './pet/pet-list/pet-list.component';
-import { AuthenticationComponent } from './user/authentication/authentication.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { NavbarComponent } from './core/navbar/navbar.component';
 import { HomeComponent } from './core/home/home.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { MaterialModule } from './material.module';
 import { AddPetComponent } from './pet/add-pet/add-pet.component';
 import { PetDetailComponent } from './pet/pet-detail/pet-detail.component';
+import { PetListComponent } from './pet/pet-list/pet-list.component';
+import { AuthenticationComponent } from './user/authentication/authentication.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +31,6 @@ import { PetDetailComponent } from './pet/pet-detail/pet-detail.component';
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
