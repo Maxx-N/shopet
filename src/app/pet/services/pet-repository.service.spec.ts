7a59be1a69dbdd2a46c6d17e1dbad920d10d20ec
maximenorval <maximenorval@gmail.com>
2022-09-23 15:13:30 +0200

    test: pet-repository service: should be created

--- src/app/pet/services/pet-repository.service.spec.ts
+++ src/app/pet/services/pet-repository.service.spec.ts
@@ -1,13 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { PetRepositoryService } from './pet-repository.service';
 
 describe('PetRepositoryService', () => {
   let service: PetRepositoryService;
+  let http: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PetRepositoryService],
+    });
     service = TestBed.inject(PetRepositoryService);
+    http = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
