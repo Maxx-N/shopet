9c7b186e6630a66a52c6dd873148aa6fd297caf1
maximenorval <maximenorval@gmail.com>
2022-09-23 17:50:56 +0200

    test: navbar component: should create

--- src/app/core/navbar/navbar.component.spec.ts
+++ src/app/core/navbar/navbar.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { UiService } from 'src/app/shared/ui.service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let uiServiceSpy: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    uiServiceSpy = jasmine.createSpy('UiService');
+    TestBed.configureTestingModule({
+      declarations: [NavbarComponent],
+      imports: [HttpClientTestingModule, SharedModule, RouterTestingModule],
+      providers: [{ provide: UiService, useValue: uiServiceSpy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
