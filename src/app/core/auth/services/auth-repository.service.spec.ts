43065b4333b2eb3d6d7f5fbab5df9793d5fac104
maximenorval <maximenorval@gmail.com>
2022-09-23 11:13:51 +0200

    test: auth-repository service sould be created

--- src/app/core/auth/services/auth-repository.service.spec.ts
+++ src/app/core/auth/services/auth-repository.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthRepositoryService } from './auth-repository.service';
 
@@ -6,7 +7,9 @@ describe('AuthRepositoryService', () => {
   let service: AuthRepositoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AuthRepositoryService);
   });
 
