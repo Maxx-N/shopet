5bd7ef672590b6e6cf712f1e64e7925d8562167d
maximenorval <maximenorval@gmail.com>
2022-09-23 17:34:12 +0200

    test: pet-list component: should have the correct pets & should have the correct pet status

--- src/app/pet/components/pet-list/pet-list.component.spec.ts
+++ src/app/pet/components/pet-list/pet-list.component.spec.ts
@@ -6,7 +6,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { firstValueFrom, of } from 'rxjs';
 
+import { IPet } from '../../models/pet.model';
 import { PetRepositoryService } from '../../services/pet-repository.service';
 import { PetService } from '../../services/pet.service';
 import { PetListComponent } from './pet-list.component';
@@ -18,10 +20,12 @@ describe('PetListComponent', () => {
   let petServiceSpy: any;
 
   beforeEach(() => {
+    petServiceSpy = jasmine.createSpyObj('PetService', ['getAllPetStatus']);
+    petServiceSpy.getAllPetStatus.and.returnValue(status);
     petRepositorySpy = jasmine.createSpyObj('PetRepositoryService', [
       'getPetsByStatus$',
     ]);
-    petServiceSpy = jasmine.createSpyObj('PetService', ['getAllPetStatus']);
+    petRepositorySpy.getPetsByStatus$.and.returnValue(of(getPets()));
 
     TestBed.configureTestingModule({
       imports: [
@@ -48,4 +52,35 @@ describe('PetListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the correct pet status', () => {
+    const status = getPetsStatus();
+    for (let i = 0; i < component.petStatus.length; i++) {
+      expect(component.petStatus[i]).toEqual(status[i]);
+    }
+  });
+
+  it('should have the correct pets', async () => {
+    const pets = getPets();
+
+    const result: IPet[] = await firstValueFrom(component.pets$);
+
+    for (let i = 0; i < pets.length; i++) {
+      for (const key of Object.keys(pets[i])) {
+        expect((result[i] as any)[key]).toEqual((pets[i] as any)[key]);
+      }
+    }
+  });
+
+  const getPetsStatus = (): string[] => {
+    return ['sold', 'pending', 'available'];
+  };
+
+  const getPets = (): IPet[] => {
+    return [
+      { id: 1, imageUrl: 'testUrl1', name: 'pet1', status: 'available' },
+      { id: 2, imageUrl: 'testUrl2', name: 'pet2', status: 'available' },
+      { id: 3, imageUrl: 'testUrl3', name: 'pet3', status: 'available' },
+    ];
+  };
 });
