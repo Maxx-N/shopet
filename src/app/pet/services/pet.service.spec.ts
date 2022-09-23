f0ffecfadff180475f68ec5e6448dcc440ec0ab7
maximenorval <maximenorval@gmail.com>
2022-09-23 16:53:55 +0200

    test: pet service: should get a copy of pet status

--- src/app/pet/services/pet.service.spec.ts
+++ src/app/pet/services/pet.service.spec.ts
@@ -15,4 +15,14 @@ describe('PetService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get a copy of pet status', () => {
+    const result: string[] = service.getAllPetStatus();
+
+    const expectedObj = { 0: 'sold', 1: 'pending', 2: 'available' };
+
+    for (const key of Object.keys(expectedObj)) {
+      expect(result[+key] ).toEqual((expectedObj as any)[key])
+    }
+  });
 });
