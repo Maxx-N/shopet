162744cf99b5c13ee0b2da28f52afe1c26bad6ea
maximenorval <maximenorval@gmail.com>
2022-09-23 18:02:46 +0200

    test: navbar component: should know the screen size

--- src/app/core/navbar/navbar.component.spec.ts
+++ src/app/core/navbar/navbar.component.spec.ts
@@ -5,18 +5,28 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { NavbarComponent } from './navbar.component';
 import { UiService } from 'src/app/shared/ui.service';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ResponsiveService } from '../responsive/responsive.service';
+import { firstValueFrom, of } from 'rxjs';
+import { ScreenSize } from '../responsive/screen-size.enum';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
   let uiServiceSpy: any;
+  let responsiveServiceSpy: any;
 
   beforeEach(() => {
     uiServiceSpy = jasmine.createSpy('UiService');
+    responsiveServiceSpy = jasmine.createSpyObj('ResponsiveService', [], {
+      currentScreenSize$: of(ScreenSize.S),
+    });
     TestBed.configureTestingModule({
       declarations: [NavbarComponent],
       imports: [HttpClientTestingModule, SharedModule, RouterTestingModule],
-      providers: [{ provide: UiService, useValue: uiServiceSpy }],
+      providers: [
+        { provide: UiService, useValue: uiServiceSpy },
+        { provide: ResponsiveService, useValue: responsiveServiceSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
@@ -27,4 +37,11 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should know the screen size', async () => {
+    const res: boolean = await firstValueFrom(component.isScreenSmall$);
+    expect(res).toBeTrue();
+  });
+
+  it('should know if user is authenticated', () => {});
 });
