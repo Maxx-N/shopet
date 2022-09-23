d70a1f3d3005bf9afceb54df0edbc493261c91ea
maximenorval <maximenorval@gmail.com>
2022-09-23 09:38:28 +0200

    renaming the app 'shopet'

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/draft'),
+      dir: require('path').join(__dirname, './coverage/shopet'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'draft'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('draft');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('draft app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('shopet app is running!');
   });
 });
