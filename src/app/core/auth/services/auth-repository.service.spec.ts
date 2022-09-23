30cdf4f7f3968810a3a9ad964785da0e81ae5efe
maximenorval <maximenorval@gmail.com>
2022-09-23 14:42:24 +0200

    test: auth-repository service: should not sign up when username is not available

--- src/app/core/auth/services/auth-repository.service.spec.ts
+++ src/app/core/auth/services/auth-repository.service.spec.ts
@@ -1,10 +1,9 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom, of, throwError } from 'rxjs';
 
 import { AuthRepositoryService } from './auth-repository.service';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { of, take, throwError } from 'rxjs';
-
 import { IUserDto } from '../models/user-dto.model';
 
 describe('AuthRepositoryService', () => {
@@ -25,7 +24,7 @@ describe('AuthRepositoryService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should sign up when username is available', () => {
+  it('should sign up when username is available', async () => {
     let alreadyCalled = false;
     spyOn(http, 'get').and.callFake(() => {
       if (alreadyCalled) {
@@ -34,19 +33,21 @@ describe('AuthRepositoryService', () => {
       alreadyCalled = true;
       return throwError(() => new Error('error'));
     });
-
     spyOn(http, 'post').and.returnValue(of({}));
 
-    service
-      .signup$(userDto)
-      .pipe(take(1))
-      .subscribe((usr) => {
-        expect(usr.username).toEqual(userDto.username);
-        expect(usr.password).toEqual(userDto.password);
-      });
+    const usr: IUserDto = await firstValueFrom(service.signup$(userDto));
+
+    expect(usr.username).toEqual(userDto.username);
+    expect(usr.password).toEqual(userDto.password);
   });
 
-  it('should not sign up when username is not available', () => {});
+  it('should not sign up when username is not available', async () => {
+    spyOn(http, 'get').and.returnValue(of({}));
+
+    await expectAsync(
+      firstValueFrom(service.signup$(userDto))
+    ).toBeRejectedWith(Error('Username is already taken.'));
+  });
 
   it('should log in when username exists and password is correct', () => {});
 
