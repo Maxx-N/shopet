227d9eea5966ee4ee7fad4ed2871610eef252f6f
maximenorval <maximenorval@gmail.com>
2022-09-23 18:32:31 +0200

    test: pet-detail component: should create

--- src/app/pet/components/pet-detail/pet-detail.component.spec.ts
+++ src/app/pet/components/pet-detail/pet-detail.component.spec.ts
@@ -1,16 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+import { SharedModule } from 'src/app/shared/shared.module';
 import { PetDetailComponent } from './pet-detail.component';
 
 describe('PetDetailComponent', () => {
   let component: PetDetailComponent;
   let fixture: ComponentFixture<PetDetailComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PetDetailComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PetDetailComponent],
+      imports: [MatDialogModule, SharedModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: { pet: { name: 'any name' } } },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PetDetailComponent);
     component = fixture.componentInstance;
