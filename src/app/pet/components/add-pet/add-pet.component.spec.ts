32bd69832be5c0f06890f756880aba5318532619
maximenorval <maximenorval@gmail.com>
2022-09-23 18:27:01 +0200

    test: add-pet component: testing posting cases

--- src/app/pet/components/add-pet/add-pet.component.spec.ts
+++ src/app/pet/components/add-pet/add-pet.component.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PetRepositoryService } from '../../services/pet-repository.service';
 import { PetService } from '../../services/pet.service';
 import { AddPetComponent } from './add-pet.component';
 import { UiService } from 'src/app/shared/ui.service';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddPetComponent', () => {
   let component: AddPetComponent;
@@ -15,13 +17,23 @@ describe('AddPetComponent', () => {
   let uiServiceSpy: any;
 
   beforeEach(() => {
-    petRepositorySpy = jasmine.createSpy('PetRepositoryService');
+    petRepositorySpy = jasmine.createSpyObj('PetRepositoryService', [
+      'postPet$',
+    ]);
     petServiceSpy = jasmine.createSpyObj('PetService', ['getAllPetStatus']);
-    uiServiceSpy = jasmine.createSpy('UiService');
+    uiServiceSpy = jasmine.createSpyObj('UiService', ['show3secSnackBar']);
+
+    petRepositorySpy.postPet$.and.returnValue(of({ status: 'test' }));
 
     TestBed.configureTestingModule({
       declarations: [AddPetComponent],
-      imports: [BrowserAnimationsModule, SharedModule],
+      imports: [
+        BrowserAnimationsModule,
+        SharedModule,
+        RouterTestingModule.withRoutes([
+          { path: 'pet/index/test', component: {} as any },
+        ]),
+      ],
       providers: [
         { provide: PetRepositoryService, useValue: petRepositorySpy },
         { provide: PetService, useValue: petServiceSpy },
@@ -36,5 +48,45 @@ describe('AddPetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should try to post a pet if form is valid', () => {
+    component.form.setValue({
+      name: 'test',
+      status: 'pending',
+      imageUrl: 'test',
+    });
+    component.onAddPet();
+    expect(petRepositorySpy.postPet$).toHaveBeenCalled();
+  });
+
+  it('should not try to post a pet if form as empty name', () => {
+    component.form.setValue({
+      name: '',
+      status: 'pending',
+      imageUrl: 'test',
+    });
+    component.onAddPet();
+    expect(petRepositorySpy.postPet$).not.toHaveBeenCalled();
+  });
+
+  it('should not try to post a pet if form as empty status', () => {
+    component.form.setValue({
+      name: 'test',
+      status: '',
+      imageUrl: 'test',
+    });
+    component.onAddPet();
+    expect(petRepositorySpy.postPet$).not.toHaveBeenCalled();
+  });
+
+  it('should not try to post a pet if form as empty image URL', () => {
+    component.form.setValue({
+      name: 'test',
+      status: 'pending',
+      imageUrl: '',
+    });
+    component.onAddPet();
+    expect(petRepositorySpy.postPet$).not.toHaveBeenCalled();
   });
 });
