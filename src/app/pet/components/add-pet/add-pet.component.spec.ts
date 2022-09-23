1c3f413a25a6f6454b8a93c9a070c98091306c83
maximenorval <maximenorval@gmail.com>
2022-09-23 18:11:22 +0200

    test: add-pet component: should create

--- src/app/pet/components/add-pet/add-pet.component.spec.ts
+++ src/app/pet/components/add-pet/add-pet.component.spec.ts
@@ -1,16 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { PetRepositoryService } from '../../services/pet-repository.service';
+import { PetService } from '../../services/pet.service';
 import { AddPetComponent } from './add-pet.component';
+import { UiService } from 'src/app/shared/ui.service';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddPetComponent', () => {
   let component: AddPetComponent;
   let fixture: ComponentFixture<AddPetComponent>;
+  let petRepositorySpy: any;
+  let petServiceSpy: any;
+  let uiServiceSpy: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddPetComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    petRepositorySpy = jasmine.createSpy('PetRepositoryService');
+    petServiceSpy = jasmine.createSpyObj('PetService', ['getAllPetStatus']);
+    uiServiceSpy = jasmine.createSpy('UiService');
+
+    TestBed.configureTestingModule({
+      declarations: [AddPetComponent],
+      imports: [BrowserAnimationsModule, SharedModule],
+      providers: [
+        { provide: PetRepositoryService, useValue: petRepositorySpy },
+        { provide: PetService, useValue: petServiceSpy },
+        { provide: UiService, useValue: uiServiceSpy },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddPetComponent);
     component = fixture.componentInstance;
