ced391d390fe7117dd3201913292ffcf61c76e35
maximenorval <maximenorval@gmail.com>
2022-09-23 14:16:24 +0200

    test: auth-repository service: should sign up when username is available

--- src/app/core/auth/services/auth-repository.service.spec.ts
+++ src/app/core/auth/services/auth-repository.service.spec.ts
@@ -2,22 +2,49 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthRepositoryService } from './auth-repository.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { of, take, throwError } from 'rxjs';
+
+import { IUserDto } from '../models/user-dto.model';
 
 describe('AuthRepositoryService', () => {
   let service: AuthRepositoryService;
+  let http: HttpClient;
+  let userDto: IUserDto;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(AuthRepositoryService);
+    http = TestBed.inject(HttpClient);
+    userDto = { username: 'bob', password: '123' };
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should sign up when username is available', () => {});
+  it('should sign up when username is available', () => {
+    let alreadyCalled = false;
+    spyOn(http, 'get').and.callFake(() => {
+      if (alreadyCalled) {
+        return of(userDto as any);
+      }
+      alreadyCalled = true;
+      return throwError(() => new Error('error'));
+    });
+
+    spyOn(http, 'post').and.returnValue(of({}));
+
+    service
+      .signup$(userDto)
+      .pipe(take(1))
+      .subscribe((usr) => {
+        expect(usr.username).toEqual(userDto.username);
+        expect(usr.password).toEqual(userDto.password);
+      });
+  });
 
   it('should not sign up when username is not available', () => {});
 
